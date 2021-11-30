bd92305c07ef0334e147c2a229593e1a57619680
Lawuhl14 <89604339+Lawuhl14@users.noreply.github.com>
2021-11-29 23:43:43 -0600

    Update user.py

--- user.py
+++ user.py
@@ -1,1 +1,13 @@
 #file for user.py
+
+#Create table
+conn.execute('''CREATE TABLE users 
+		(userId INTEGER PRIMARY KEY, 
+		password String,
+		userName String,
+		firstName String,
+		lastName String,
+		address1 Strjng,
+    admin bool, 
+		payment integer
+		)''')
